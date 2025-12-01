d3d42976bfa0424e17ee6aa946fe3b6c822b5c34
Jonathan <byersj920@gmail.com>
2025-12-01 15:14:22 -0600

    wrong answer

--- Day01.py
+++ Day01.py
@@ -4437,19 +4437,21 @@ dial = 50
 
 for rotation in sequence:
     rotate_amount = int(rotation[1:])
-
+    
     if rotation.startswith('R'):
        dial += rotate_amount
        while dial >= 100:
            dial -= 100
+           zero_counter += 1
+           
     
     if rotation.startswith('L'):
         dial -= rotate_amount
         while dial < 0:
            dial += 100
+           zero_counter += 1
 
-    if dial == 0:
-        zero_counter += 1
+    #if dial == 0:
+        #zero_counter += 1
 
-           
 print (zero_counter)
