c08403d7980fad6675419ae5c919281c88516ae0
Jonathan <byersj920@gmail.com>
2025-12-02 09:28:41 -0600

    returns correct answer

--- ID_Fixins.py
+++ ID_Fixins.py
@@ -1,4 +1,5 @@
 from ID_Collection import *
+import math
 
 invalid_IDs = []
 
@@ -9,5 +10,22 @@ for product in products:
 
         if len(stringy_ID) % 2 == 0:
             new_list = list(stringy_ID)
-            print(new_list)           
+
+            halfway_point = math.floor(len(new_list)/2)
+
+            first_half = new_list[:halfway_point]
+            first_half = int("".join(first_half))
+            second_half = new_list[halfway_point:]
+            second_half = int("".join(second_half))
+
+            if first_half - second_half == 0:
+                invalid_IDs.append(ID)
+
+invalid_ID_total = 0
+
+for ID in invalid_IDs:
+    invalid_ID_total += ID
+
+print (invalid_ID_total)
+            
 
