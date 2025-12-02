086d12ae3d1ae61cd7446ba4a09377eb32904728
Jonathan <byersj920@gmail.com>
2025-12-02 09:05:08 -0600

    code now splits digits

--- ID_Fixins.py
+++ ID_Fixins.py
@@ -1,3 +1,13 @@
 from ID_Collection import *
 
-print(products)
+invalid_IDs = []
+
+for product in products:
+    for ID in range (product["min"], product["max"]+1):
+        
+        stringy_ID = str(ID)
+
+        if len(stringy_ID) % 2 == 0:
+            new_list = list(stringy_ID)
+            print(new_list)           
+
