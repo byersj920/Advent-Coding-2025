53e1f819183719d278294c988b66cc0b7cf5304b
Jonathan <byersj920@gmail.com>
2025-12-05 15:39:23 -0600

    Succesful Puzzle

--- Pattern_Checking.py
+++ Pattern_Checking.py
@@ -1,37 +1,17 @@
 from ID_Collection import *
-import math
 from Has_Pattern import *
 
-invalid_IDs = []
+invalid_ID_total = 0
 
 
 for product in products:
 
     for ID in range (product["min"], product["max"]+1):
         
-        stringy_ID = str(ID)
-        id_size = len(stringy_ID)
+        if has_pattern(ID) == True:
+            invalid_ID_total += ID
 
-        for num in range (1, id_size+1):
-            number_check = id_size
 
-        if len(stringy_ID) % 2 == 0:
-            new_list = list(stringy_ID)
-
-            halfway_point = math.floor(len(new_list)/2)
-
-            first_half = new_list[:halfway_point]
-            first_half = int("".join(first_half))
-            second_half = new_list[halfway_point:]
-            second_half = int("".join(second_half))
-
-            if first_half - second_half == 0:
-                invalid_IDs.append(ID)
-
-invalid_ID_total = 0
-
-for ID in invalid_IDs:
-    invalid_ID_total += ID
 
 print (invalid_ID_total)
             
