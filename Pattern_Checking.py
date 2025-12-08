88b1cfcfa80c6ef5b35d58ae3a46886e680426d0
Jonathan <byersj920@gmail.com>
2025-12-08 14:12:00 -0600

    Puzzle Complete

--- Pattern_Checking.py
+++ Pattern_Checking.py
@@ -1,17 +1,31 @@
-from ID_Collection import *
-from Has_Pattern import *
+from Batteries import *
 
-invalid_ID_total = 0
+total_voltage = 0
+voltage_list = []
+
+for pack in battery_packs:
+    stringy_battery = str(pack)
+    digit_list = []
+    highest_voltage = 0
 
 
-for product in products:
+    for digit in stringy_battery:
+        digit_list.append(digit)
 
-    for ID in range (product["min"], product["max"]+1):
+    starting_range = 0
+    max_voltage = 0
+
+    for digit in digit_list:
+
+        for num in range (starting_range, len(digit_list)-1):
+            voltage_combo = int(digit_list[starting_range] + digit_list[num+1])
+            if voltage_combo > max_voltage:
+                max_voltage = voltage_combo
         
-        if has_pattern(ID) == True:
-            invalid_ID_total += ID
+        starting_range += 1
+    
+    total_voltage += max_voltage
 
 
-
-print (invalid_ID_total)
-            
+print(total_voltage)
+    
