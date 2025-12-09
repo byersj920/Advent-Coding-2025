698aa5297a8d6cab704e6bc123cfcd11484d7552
Jonathan <byersj920@gmail.com>
2025-12-09 09:57:59 -0600

    Puzzle Solved

--- Find_Highest_Digit.py
+++ Find_Highest_Digit.py
@@ -2,12 +2,12 @@
 
 def find_highest_digit (digits: list, starting_point: int, stopping_point: int):
 
-    results = {"highest_digit": 0, "new_starting_point": 0, "new_stopping_point": stopping_point -1}
+    results = {"highest_digit": 0, "new_starting_point": starting_point, "new_stopping_point": stopping_point + 1}
 
     for r in range (starting_point, stopping_point):
         if digits[r] > results["highest_digit"]:
             results["highest_digit"] = digits[r]
-            results["new_starting_point"] += 1
+            results["new_starting_point"] = r + 1
         
         if results["highest_digit"] == 9:
             return results
--- Voltage_Checking.py
+++ Voltage_Checking.py
@@ -1,29 +1,30 @@
 from Batteries import *
+from Find_Highest_Digit import *
 
 
 total_voltage = 0
 
 for pack in battery_packs:
     stringy_battery = str(pack)
-    highest_voltage = 0
-    stopping_point = len(stringy_battery) - 13
     starting_point = 0
+    stopping_point = len(stringy_battery) - 11
+    highest_digits = []
 
     digits = []
     for digit in stringy_battery:
         digits.append(int(digit))
     
     
+    for r in range (0, 12):
+        results = find_highest_digit(digits, starting_point, stopping_point)
+        highest_digits.append(str(results['highest_digit']))
+        starting_point = results['new_starting_point']
+        stopping_point = results['new_stopping_point']
+
 
     
-
-        
-
-
-
-
-    
-    total_voltage += highest_voltage
+    highest_voltage = "".join(highest_digits)
+    total_voltage += int(highest_voltage)
 
  
 print(total_voltage)
