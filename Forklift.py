0df23e227adbb36c6fb435a72e1c94091bbdfee8
Jonathan <byersj920@gmail.com>
2025-12-11 14:13:39 -0600

    started changes for part 2 accidentally

--- Forklift.py
+++ Forklift.py
@@ -1,29 +1,30 @@
-from Wrapping_Paper import *
 from Check_For_Paper import *
 
-#This is the shelf we are currently looking at.
-row = 0
-accessible_papers = 0
 
-for shelf in shelves:
+def forklift(shelves: list):
+    row = 0
+    accessible_papers = 0
 
-    if row == 0:
-        above_shelf = ""
-    else:
-        above_shelf = shelves[row-1]
+    for shelf in shelves:
 
-
-    try:
-        below_shelf = shelves[row+1]
-    except Exception:
-        below_shelf = ""
+        if row == 0:
+            above_shelf = ""
+        else:
+            above_shelf = shelves[row-1]
 
 
-    accessible_papers += check_for_paper(above_shelf,shelf,below_shelf)
+        try:
+            below_shelf = shelves[row+1]
+        except Exception:
+            below_shelf = ""
 
-    row += 1
+
+        accessible_papers += check_for_paper(above_shelf,shelf,below_shelf)
+
+        row += 1
 
 
-print(accessible_papers)
+    print(accessible_papers)
+
 
 
