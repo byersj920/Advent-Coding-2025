c99eadc17042ea3ea3aebee718c9cf56d17bbc50
Jonathan <byersj920@gmail.com>
2025-12-16 12:33:33 -0600

    created puzzle input sorter

---
+++ Puzzle_Input.py
@@ -0,0 +1,21 @@
+puzzle_input = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32"""
+
+
+puzzle_input = puzzle_input.split('\n\n')
+
+valid_ranges = puzzle_input[0].split('\n')
+ingredients = puzzle_input[1].split('\n')
+
+
+
+
