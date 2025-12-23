736f9661b8aed972cc0cf716cd55598d52cac480
Jonathan <byersj920@gmail.com>
2025-12-23 16:36:58 -0600

    adjusting when numbers get inserted

--- Puzzle_Input.py
+++ Puzzle_Input.py
@@ -7,7 +7,6 @@ puzzle_input = """3-5
 puzzle_input = puzzle_input.split('\n\n')
 
 valid_ranges_strings = puzzle_input[0].split('\n')
-ingredients_strings = puzzle_input[1].split('\n')
 
 valid_ranges = []
 ingredients = []
@@ -19,9 +18,5 @@ for id_range in valid_ranges_strings:
     new_range["min"] = int(splitRange[0])
     new_range["max"] = int(splitRange[1])
     valid_ranges.append(new_range)
-
-for ingredient in ingredients_strings:
-    ingredients.append(int(ingredient))
-
 
 
