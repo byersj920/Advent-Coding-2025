05143918fe3504e2df815aa3fe8fea65d44b21b5
Jonathan <byersj920@gmail.com>
2026-01-08 12:41:41 -0600

    Files changed

--- Ingredient_Checker.py
+++ Ingredient_Checker.py
@@ -1,14 +1,11 @@
 from Puzzle_Input import *
 
-valid_IDs = []
+total_ranges = [{'min': 0, 'max': 0}]
 
 for id_range in valid_ranges:
-    for num in range(id_range['min'], id_range['max']+1):
-        
-        if num in valid_IDs:
-            continue
-        else:
-            valid_IDs.append(num)
+    for checked_range in total_ranges:
+        if checked_range['min'] > id_range['min']:
+            checked_range
 
 print(len(valid_IDs))
 
