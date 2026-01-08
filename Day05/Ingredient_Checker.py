c807d1b8482fd6e779017aef11e5cea8fd0143a0
Jonathan <byersj920@gmail.com>
2026-01-08 15:05:18 -0600

    Started creating range checker

--- Day05/Ingredient_Checker.py
+++ Day05/Ingredient_Checker.py
@@ -2,26 +2,26 @@ from Puzzle_Input import *
 
 final_pantry = []
 final_pantry.append(valid_ranges[0])
+golden_range = valid_ranges[1]
+temp_pantry = []
 
 for id_range in valid_ranges:
+
+    if id_range['min'] <= golden_range['min'] and id_range['max'] >= golden_range['max']:
+        golden_range = id_range
+        print(golden_range)
+        continue
     
-    min_value_inserted = False
-    max_value_inserted = False
 
-    for shelf in final_pantry:
-
-        if id_range['min'] <= shelf['min'] and id_range['max'] >= shelf['min']:
-            shelf['min'] = id_range['min']
-            min_value_inserted = True
-            print(final_pantry)
-        if id_range['max'] >= shelf['max'] and id_range['min'] <= shelf['max']:
-            shelf['max'] = id_range['max']
-            max_value_inserted = True
-            print(final_pantry)
-
-    if min_value_inserted == False and max_value_inserted == False:
-        final_pantry.append(id_range)
-        print(final_pantry)
+    if id_range['min'] < golden_range['min'] and id_range['max'] <= golden_range['max'] and id_range['max'] >= golden_range['min']:
+        golden_range['min'] = id_range['min']
+        print(golden_range)
+    
+    if id_range['max'] > golden_range['max'] and id_range['min'] <= golden_range['max'] and id_range['min'] >= golden_range['min']:
+        golden_range['max'] = id_range['max']
+        print(golden_range)
+    
 
 
-print(final_pantry)
+print(f'The final range is {golden_range}.')
+
